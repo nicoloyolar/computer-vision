6bb955ccb8f2da7aa4b1c8f3f199546868abd9f5
Luis <sanmaglass@gmail.com>
2024-08-10 15:54:58 -0400

    Cambios conexion caamara con py

--- conexionIp.py
+++ conexionIp.py
@@ -2,36 +2,31 @@ import cv2
 
 # URL RTSP de la cámara
 rtsp_url = "rtsp://admin:admin123@192.168.31.108:554/cam/realmonitor?channel=5&subtype=0"
-
-# Conectar a la cámara
 cap = cv2.VideoCapture(rtsp_url)
 
 if not cap.isOpened():
-    print("No se pudo conectar a la cámara.")
+    print("Acceso denegado.")
 else:
-    print("Conectado a la cámara IP Proyecto St.")
+    print("Conectado a la cámara IP")
 
-# Bucle para leer frames de la cámara
 while True:
     ret, frame = cap.read()
     if not ret:
         print("No se pudo leer el frame.")
         break
 
-    # Redimensionar el frame para que no se vea tan grande
+    # dimensiones frame
     frame_resized = cv2.resize(frame, (640, 480))
 
     # Convertir a escala de grises
     gray_frame = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2GRAY)
 
-    # Aplicar umbralización (después de convertir a escala de grises)
+    # unmbralizacion)
     _, thresh_frame = cv2.threshold(gray_frame, 128, 255, cv2.THRESH_BINARY)
 
-    # Mostrar el video en escala de grises y el video umbralizado
     cv2.imshow("Video en Escala de Grises", gray_frame)
     cv2.imshow("Video Umbralizado", thresh_frame)
 
-    # Salir del bucle si se presiona la tecla 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
